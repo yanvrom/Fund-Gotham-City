b7a0f18c49227b0dabdd8ffb1e4aad4ae3459ca4
Yan Romano <103431150+yanvrom@users.noreply.github.com>
2022-06-17 04:36:19 -0300

    organizando main e fazendo o começo da questao

--- main.py
+++ main.py
@@ -1,54 +1,65 @@
 from importandodados import *
 from functions import calculate
+
+
 start_date = 0
 end_date = 0
 frequency = 0
 
+
 #entradas
 while start_date not in dias:
-    start_date = input('Start date: ')
+        start_date = input('Start date: ')
 
 while end_date not in dias:
     end_date = input('End date: ')
     if end_date in dias:
         if dias.index(end_date) < dias.index(start_date):
             end_date = 0
-        
 
 capital = float(input('Capital: '))
-
+capital0 = capital
 while frequency not in ['day', 'month', 'year']:
     frequency = input('Frequency: ')
 
-#infosguardadas dia, capital, ganho
-retorno = [{'Date':start_date, 'Capital':capital, 'Amount earned':0}]
 
-capital0 = capital
-indiceinicial = dias.index(start_date)
-indicefinal = dias.index(end_date)
 
-if frequency == 'day':
-    for i in range(indiceinicial+1, indicefinal+1):
-        capital = calculate(capital, float(taxas[i])/100, 1)
-        retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
+def main(start_date, end_date, capital, frequency):
+    global capital0
 
-elif frequency == 'month':
-    for i in range(indiceinicial+1, indicefinal+1):
-        capital = calculate(capital, float(taxas[i])/100, 1)
-        if i != indicefinal:
-            if dias[i][4] != dias[i+1][4]:
-                retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
-        else:
+    #infosguardadas dia, capital, ganho
+    retorno = [{'Date':start_date, 'Capital':capital, 'Amount earned':0}]
+
+    indiceinicial = dias.index(start_date)
+    indicefinal = dias.index(end_date)
+
+    if frequency == 'day':
+        for i in range(indiceinicial+1, indicefinal+1):
+            capital = calculate(capital, float(taxas[i])/100, 1)
             retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
 
-elif frequency == 'year':
-    for i in range(indiceinicial+1, indicefinal+1):
-        capital = calculate(capital, float(taxas[i])/100, 1)
-        if i != indicefinal:
-            if dias[i][9] != dias[i+1][9]:
+    elif frequency == 'month':
+        for i in range(indiceinicial+1, indicefinal+1):
+            capital = calculate(capital, float(taxas[i])/100, 1)
+            if i != indicefinal:
+                if dias[i][4] != dias[i+1][4]:
+                    retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
+            else:
                 retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
-        else:
-            retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
+
+    elif frequency == 'year':
+        for i in range(indiceinicial+1, indicefinal+1):
+            capital = calculate(capital, float(taxas[i])/100, 1)
+            if i != indicefinal:
+                if dias[i][9] != dias[i+1][9]:
+                    retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
+            else:
+                retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
+    return retorno
+
+
+retorno = main(start_date, end_date, capital, frequency)
+
 
 with open(f'retorno/start({start_date[0:2]}-{start_date[3:5]}-{start_date[6-10]})_end({end_date[0:2]}-{end_date[3:5]}-{end_date[6-10]})_{capital0}_{frequency}.txt', 'w') as arquivo:
     arquivo.write(str(retorno))
---
+++ questaodos500dias.py
@@ -0,0 +1,9 @@
+# from main import main
+from importandodados import *
+from functions import calculate
+
+inicio = dias.index('03/01/2000')
+fim  = dias.index('31/03/2022')
+diaspossiveis = dias[inicio:(fim+1)]
+#Como o valor inicial do capital não importa, usarei 10000.
+
