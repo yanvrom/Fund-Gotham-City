f9fcbaee99e1a92bb51154b1604c15f5f4887bfa
Yan Romano <103431150+yanvrom@users.noreply.github.com>
2022-06-17 00:04:00 -0300

    adicionando escrita de arquivo para guardar dados

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Fund-Gotham-City
 Gotham City case of giant steps Intership 2022Q2
+
+To run the program, just run the main.py file and the result will be saved in the retorno folder
+
+As I was taking end-of-semester exams and I felt sick on the last day of delivery, I didn't have much time, but I will continue to update during the dawn of 06/17/2022 to complete
--- main.py
+++ main.py
@@ -10,12 +10,15 @@ capital = float(input('Capital: '))
 frequency = input('Frequency: ')
 
 #infosguardadas dia, capital, ganho
-retorno = []
+retorno = [{'Date':start_date, 'Capital':capital, 'Amount earned':0}]
 
 capital0 = capital
 if frequency == 'day':
     indiceinicial = dias.index(start_date)
     indicefinal = dias.index(end_date)
-    for i in range(indiceinicial, indicefinal+1):
+    for i in range(indiceinicial+1, indicefinal+1):
         capital = calculate(capital, float(taxas[i])/100, 1)
         retorno.append({'Date':dias[i], 'Capital':capital, 'Amount earned':(capital-capital0)})
+
+with open(f'{capital0}{frequency}.txt', 'w') as arquivo:
+    arquivo.write(str(retorno))
